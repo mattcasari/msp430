3026af64c380224272a7dcdf2ac2c071edc83694
Matt Casari <mjcasari@gmail.com>
2015-06-15 21:14:11 -0700

    first commit

---
+++ SOS_Blinky/Debug/main.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/TI/ccsv6/ccs_base/msp430/include/msp430.h
+main.obj: C:/TI/ccsv6/ccs_base/msp430/include/msp430g2231.h
+main.obj: C:/TI/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdint.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdio.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/linkage.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdarg.h
+
+../main.c: 
+C:/TI/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/TI/ccsv6/ccs_base/msp430/include/msp430g2231.h: 
+C:/TI/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdint.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdio.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/linkage.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdarg.h: 
---
+++ SOS_Blinky/main.c
@@ -0,0 +1,97 @@
+/**********************************************************
+ * SOS Blinky
+ *********************************************************/
+
+/*********************************************************
+ * File:  		main.c
+ * Author: 		Matt Casari
+ * Description: This file contains code to blink the green
+ * 				LED on the MSP430 Launchpad with no interrupts
+ * 				driven directly from main loop with while()
+ * 				countdown.
+ *
+ * History: 	MJC - 6/12/2015
+ *
+ * Notes:
+ *********************************************************/
+
+#include <msp430.h>
+#include <intrinsics.h>
+#include <stdint.h>
+#include <stdio.h>
+/*
+ * main.c
+ */
+
+
+/***************** Function Prototypes ********************/
+void initLEDs(void);
+void sosUpdate(void);
+
+
+/************************ Main ****************************/
+int main(void) {
+
+	volatile int8_t i = 0;
+
+    WDTCTL = WDTPW + WDTHOLD;	// Stop watchdog timer
+
+    // Initialize Peripherals
+    initLEDs();
+
+    // Main loop
+    for(;;) {
+    		volatile unsigned int i;	// volatile to prevent optimization
+
+    		// Update the LED
+    		sosUpdate();
+
+    		// Countdown
+    		i = 50000;					// SW Delay
+    		do i--;
+    		while(i != 0);
+    	}
+
+	
+	return 0;
+}
+
+
+
+
+/*---------------------------------------------------------------------
+  Function Name: initLEDs
+  Description:   Initialize RED & GREEN LEDs
+  Inputs:        None
+  Returns:       None
+-----------------------------------------------------------------------*/
+void initLEDs(void) {
+
+	P1DIR |= 0x01;
+	P1DIR |= 0x40;
+	P1OUT = 0x00;
+
+	return;
+}
+
+void sosUpdate(void){
+
+	const int8_t sosvals[] = {0,0,0,0,1,0,1,0,1,0,1,1,0,1,1,0,1,1,0,1,0,1,0,1,0};
+	static int8_t i = 0;
+	static int8_t j = 0;
+
+	// Increment the value to be transmitted, wrap at max value
+	if(++j == sizeof(sosvals)){
+		j = 0;
+		P1OUT ^= 0x01;
+	}
+
+	// Write to LED
+	if(sosvals[j] == 1){
+		P1OUT |= 0x40;
+	} else {
+		P1OUT &= 0xBF;
+	}
+
+	//printf("%d",i);
+}
